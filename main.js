6e5f06bae5dd298517c567333f7ee4d10886bcbe
Your Name <you@example.com>
2024-03-16 21:10:56 -0400

    updating content

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
-// Function to handle click event on images
+
 function enlargeImage(event) {
     const clickedImage = event.target;
     clickedImage.classList.toggle('enlarged');
 }
 
-// Fetch data from the API
+
 fetch('https://api.artic.edu/api/v1/artworks')
     .then(response => response.json())
     .then(data => {
-// Process the data and display artworks
+
         const artworks = data.data;
         const gallery = document.querySelector('.gallery');
 
@@ -17,7 +17,7 @@ fetch('https://api.artic.edu/api/v1/artworks')
             const artworkElement = document.createElement('div');
             artworkElement.classList.add('artwork');
 
-                // Convert title to lowercase and replace spaces with underscores
+            
             const title = artwork.title.toLowerCase().replace(/\s/g, '_');
             const imagePath = `images/${title}.jpg`;
 
